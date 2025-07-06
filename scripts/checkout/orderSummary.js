acdaed9a45467ec3780778617842b6f5e7ac344a
Adith <adityachavan6890@gmail.com>
2025-07-07 01:02:46 +0530

    make payment Summary Ineractive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -9,6 +9,7 @@ import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.j
 import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
+import {renderPaymentSummary} from "../checkout/paymentSummary.js"
 
 export function renderOrderSummary() {
   let cartSummaryHTML = "";
@@ -130,6 +131,8 @@ export function renderOrderSummary() {
 
       container.remove();
       updateCartQuantity();
+
+      renderPaymentSummary();
     });
   });
 
@@ -184,6 +187,7 @@ export function renderOrderSummary() {
       const { productId, deliveryOptionId } = element.dataset;
       updateDeliveryOption(productId, deliveryOptionId);
       renderOrderSummary();
+      renderPaymentSummary();
     });
   });
 }
