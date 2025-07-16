0d7664ca90c7e9bdc3dd7962c9fd0afc32bbe1b4
Adith <adityachavan6890@gmail.com>
2025-07-17 01:39:58 +0530

    EX15K

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -10,6 +10,7 @@ import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
 import {renderPaymentSummary} from "../checkout/paymentSummary.js"
+import { renderCheckoutHeader } from "./checkoutHeader.js";
 
 export function renderOrderSummary() {
   let cartSummaryHTML = "";
@@ -125,9 +126,9 @@ export function renderOrderSummary() {
       const productId = link.dataset.productId;
       removeFromCart(productId);
 
+      renderCheckoutHeader();
       renderOrderSummary();
       updateCartQuantity();
-
       renderPaymentSummary();
     });
   });
